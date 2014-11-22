7fd1663cb91e4c02b68597db3aa39f3e0d86dee1
nevelezv7 <nevelezv@gmail.com>
2014-11-22 14:17:32 -0500

    V2.1

--- www/js/index.js
+++ www/js/index.js
@@ -142,3 +142,16 @@ function buscarTiendas(idProducto){
             }, 
     });
 }
+
+
+cordova.plugins.barcodeScanner.scan(
+  function (result) {
+      alert("We got a barcode\n" +
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
+            "Cancelled: " + result.cancelled);
+  }, 
+  function (error) {
+      alert("Scanning failed: " + error);
+  }
+);
