bcec67e02a4346e0714443e74128542d930199fa
nevelezv7 <nevelezv@gmail.com>
2014-11-22 11:15:58 -0500

    v.1.1

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -58,6 +40,8 @@ var app = {
             var image = document.getElementById('MyImg');
             image.src = imageURI;
             image.style.visibility= "visible";
+            var btn = document.getElementById('btnBuscarXFoto');
+            btn.style.visibility= "visible";
         }
 
         function onFail(message) {
@@ -78,6 +62,8 @@ var app = {
             var image = document.getElementById('MyImg');
             image.src = 'data:image/jpeg;base64,'+ dataUrl;
             image.style.visibility= "visible";
+            var btn = document.getElementById('btnBuscarXFoto');
+            btn.style.visibility= "visible";
         }
 
         function onFail(message) {
@@ -86,3 +72,12 @@ var app = {
         }
     },
 };
+
+function mostrarOpcionesCamara(){
+    var panel = document.getElementById('pantallaBienvenida');
+    panel.style.display= "none";
+
+    var panel2 = document.getElementById('opcionesCamara');
+    panel2.style.display= "block";
+
+}
